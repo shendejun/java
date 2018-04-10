460fe18f5466deec5cac081b7500aeb184e3fae3
shendejun <895017142@qq.com>
2018-04-10 09:34:59 +0100

    新增商品

--- taotao-manger/taotao-manger-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manger/taotao-manger-web/src/main/java/com/taotao/controller/ItemController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.taotao.common.pojo.EUDataGridResult;
+import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.pojo.TbItem;
 import com.taotao.service.ItemService;
 
@@ -30,6 +32,15 @@ public class ItemController {
 	@ResponseBody
 	public EUDataGridResult getItemList(Integer page,Integer rows) {
 		EUDataGridResult result=itemService.getItemList(page, rows);
+		return result;
+	}
+	
+	@RequestMapping(value="/item/save",method=RequestMethod.POST)
+	@ResponseBody
+	/*使用TbItem直接接收，要求TbItem属性与表单中的name一一对应*/
+	public TaotaoResult createItem(TbItem item)
+	{
+		TaotaoResult result= itemService.createItem(item);
 		return result;
 	}
 }
