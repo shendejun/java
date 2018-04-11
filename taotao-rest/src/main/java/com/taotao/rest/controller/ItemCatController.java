9f043d91f68c846d6ca26c1c1eb70c221e7dbcc6
shendejun <895017142@qq.com>
2018-04-11 10:55:57 +0100

    商品分类展示

---
+++ taotao-rest/src/main/java/com/taotao/rest/controller/ItemCatController.java
@@ -0,0 +1,40 @@
+package com.taotao.rest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.taotao.common.utils.JsonUtils;
+import com.taotao.rest.pojo.CatResult;
+import com.taotao.rest.service.ItemCatService;
+
+@Controller
+public class ItemCatController {
+
+	@Autowired
+	private ItemCatService itemCatService;
+//	方式一
+//	@RequestMapping(value="/itemcat/list",
+//			produces=MediaType.APPLICATION_JSON_VALUE+";charset=utf-8")
+//	@ResponseBody
+//	public String getItemCatList(String callback) {
+//		CatResult catResult = itemCatService.getItemCatList();
+//		String json = JsonUtils.objectToJson(catResult);
+//		String result = callback + "(" + json + ");";
+//		return result;
+//	}
+
+//	方式二
+	@RequestMapping("/itemcat/list")
+	@ResponseBody
+	public Object getItemCatList(String callback) {
+		CatResult catResult = itemCatService.getItemCatList();
+		MappingJacksonValue mappingJacksonValue=new MappingJacksonValue(catResult);
+		mappingJacksonValue.setJsonpFunction(callback);
+		return mappingJacksonValue;
+	}
+	
+}
