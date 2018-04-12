25d8229c080a45486742d6911d770d19d71b129a
shendejun <895017142@qq.com>
2018-04-12 04:42:11 +0100

    post乱码解决

--- taotao-portal/src/test/java/com/taotao/httpclient/HttpClientTest.java
+++ taotao-portal/src/test/java/com/taotao/httpclient/HttpClientTest.java
@@ -76,13 +76,13 @@ public class HttpClientTest {
 	public void doPostWithParam() throws Exception {
 		CloseableHttpClient httpClient = HttpClients.createDefault();
 		// 创建一个Post对象
-		HttpPost post = new HttpPost("http://localhost:8082/httpclient/post.html");
+		HttpPost post = new HttpPost("http://localhost:8082/httpclient/post.action");
 		//创建一个Entity.模拟一个表单
 		List<NameValuePair> kvList=new ArrayList<>();
-		kvList.add(new BasicNameValuePair("username", "zhangsan"));
+		kvList.add(new BasicNameValuePair("username", "张三"));
 		kvList.add(new BasicNameValuePair("password", "123"));
 		//包装成一个Entity对象
-		StringEntity entity=new UrlEncodedFormEntity(kvList);
+		StringEntity entity=new UrlEncodedFormEntity(kvList,"utf-8");
 		//设置请求内容
 		post.setEntity(entity);
 		// 执行post请求 
